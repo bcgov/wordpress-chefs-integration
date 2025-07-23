9df6eb04a8fd36d924ff900ddb2dc853c1ddcc47
Michael Haswell <Michael.Haswell@gov.bc.ca>
2025-07-23 14:27:07 -0700

    Create HttpClient to separate CHEFS API interaction from WP REST API code Add do_request() and authentication headers to work with CHEFS API Add sample.env and .env handling for providing CHEFS form ids and API keys Update RestController to be generic rather than specific to producer endpoint Fix bug in composer.json autoload not using correct path to source files Update README to explain how to start using the plugin Update CODEOWNERS to limit who is expected to review PRs

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # WordPress CHEFS Integration Plugin
+
+## Getting started
+1. Copy sample.env and rename it .env.
+2. Add form id of your producer form as `PRODUCER_FORM_ID`.
+   1. This can be found in the URL when viewing the form in CHEFS.
+3. Add form API key of your producer form as `PRODUCER_FORM_API_KEY`.
+   1. This must be generated in CHEFS for your form under Api key > Generate API Key.
--- src/Bcgov/WordpressChefsIntegration/RestController.php
+++ src/Bcgov/WordpressChefsIntegration/RestController.php
@@ -1,6 +1,8 @@
 <?php
 namespace Bcgov\WordpressChefsIntegration;
 
+use Bcgov\WordpressChefsIntegration\HttpClient;
+
 use WP_REST_Request;
 use WP_REST_Response;
 use WP_Error;
@@ -9,6 +11,34 @@ use WP_Error;
  * RestController class for handling REST API endpoints.
  */
 class RestController {
+
+    /**
+     * The HttpClient to make the requests.
+     *
+     * @var HttpClient
+     */
+    protected $http_client;
+
+    /**
+     * The endpoint this controller is for, eg. "producer", "product".
+     *
+     * @var string
+     */
+    protected $endpoint;
+
+    /**
+     * Constructor for RestController.
+     *
+     * @param HttpClient $http_client The HttpClient instance to use to make requests to CHEFS API.
+     * @param string     $endpoint    The name of the WP REST API endpoint to create.
+     */
+    public function __construct(
+        HttpClient $http_client,
+        string $endpoint
+    ) {
+        $this->http_client = $http_client;
+        $this->endpoint    = $endpoint;
+    }
 
     /**
      * Initializes the RestController Class.
@@ -27,22 +57,23 @@ class RestController {
     public function register_chefs_routes() {
         register_rest_route(
             'chefs/v1',
-            '/producer',
+            '/' . $this->endpoint,
             [
                 'methods'             => 'POST',
-                'callback'            => [ $this, 'handle_producer' ],
+                'callback'            => [ $this, 'endpoint_callback' ],
                 'permission_callback' => [ $this, 'has_permission' ],
             ]
         );
     }
 
     /**
-     * Callback function for handling requests to the producer endpoint.
+     * Callback function for handling requests to the endpoint.
      *
      * @param WP_REST_Request $request
      * @return WP_REST_Response|WP_Error
      */
-    public function handle_producer( $request ) {
+    public function endpoint_callback( $request ) {
+        // $this->http_client->do_request('submission/' . $request->submissionId);
         return rest_ensure_response( [ 'message' => 'success' ] );
     }
 
